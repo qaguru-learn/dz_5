da0c385c03dd3d9e4e21a9e8f8ae1a53765492e6
legend23 <yu@vtechnology.ru>
2024-03-03 22:59:46 +0500

    change cur_user to current_user

--- demoqa_tests/models/pages/registration_page.py
+++ demoqa_tests/models/pages/registration_page.py
@@ -9,57 +9,59 @@ class RegistrationPage:
         browser.open('/automation-practice-form')
         return self
 
-    def fill_firstname(self, cur_user: User):
-        browser.element('#firstName').should(be.blank).type(cur_user.fullname.split(' ')[0])
+    def fill_firstname(self, current_user: User):
+        browser.element('#firstName').should(be.blank).type(current_user.fullname.split(' ')[0])
         return self
 
-    def fill_lastname(self, cur_user: User):
-        browser.element('#lastName').should(be.blank).type(cur_user.fullname.split(' ')[-1])
+    def fill_lastname(self, current_user: User):
+        browser.element('#lastName').should(be.blank).type(current_user.fullname.split(' ')[-1])
         return self
 
-    def fill_email(self, cur_user: User):
-        browser.element('#userEmail').should(be.blank).type(cur_user.email)
+    def fill_email(self, current_user: User):
+        browser.element('#userEmail').should(be.blank).type(current_user.email)
         return self
 
-    def gender_make_choice(self, cur_user: User):
+    def gender_make_choice(self, current_user: User):
         choice = {Genders.MALE.value: 1, Genders.FEMALE.value: 2}
-        browser.element(f"#gender-radio-{choice[cur_user.gender]}").double_click()
+        browser.element(f"#gender-radio-{choice[current_user.gender]}").double_click()
         return self
 
-    def fill_phone_number(self, cur_user: User):
-        browser.element('#userNumber').should(be.blank).type(cur_user.phone_number)
+    def fill_phone_number(self, current_user: User):
+        browser.element('#userNumber').should(be.blank).type(current_user.phone_number)
         return self
 
-    def fill_birthday(self, cur_user: User):
+    def fill_birthday(self, current_user: User):
         browser.element('#dateOfBirthInput').click()
         browser.element(".react-datepicker__year-select").click()\
-            .element(by.text(f'{cur_user.birthday.year}')).click()
+            .element(by.text(f'{current_user.birthday.year}')).click()
         browser.element(".react-datepicker__month-select").click()\
-            .element(by.text(f'{cur_user.birthday.strftime("%B")}')).click()
-        browser.element(f".react-datepicker__week .react-datepicker__day--0{cur_user.birthday.day}").click()
+            .element(by.text(f'{current_user.birthday.strftime("%B")}')).click()
+        browser.element(f".react-datepicker__week .react-datepicker__day--0{current_user.birthday.day}").click()
         return self
 
-    def fill_subjects(self, cur_user: User):
-        browser.element('#subjectsInput').type(cur_user.subject).press_enter()
+    def fill_subjects(self, current_user: User):
+        browser.element('#subjectsInput').type(current_user.subject).press_enter()
         return self
 
-    def hobbies_make_choice(self, cur_user: User):
+    def hobbies_make_choice(self, current_user: User):
         choice = {Hobbies.SPORTS.value: 1,
                   Hobbies.READING.value: 2, Hobbies.MUSIC.value: 3}
-        browser.element(f'[for=hobbies-checkbox-{choice[cur_user.hobby]}]').click()
+        browser.element(f'[for=hobbies-checkbox-{choice[current_user.hobby]}]').click()
         return self
 
-    def upload_picture(self, cur_user: User):
-        browser.element('#uploadPicture').type(resource.path(cur_user.photo))
+    def upload_picture(self, current_user: User):
+        browser.element('#uploadPicture').type(resource.path(current_user.photo))
         return self
 
-    def fill_address(self, cur_user: User):
-        browser.element('#currentAddress').should(be.blank).type(cur_user.address)
+    def fill_address(self, current_user: User):
+        browser.element('#currentAddress').should(be.blank).type(current_user.address)
         return self
 
-    def fill_state_city(self, cur_user: User):
-        browser.element('#react-select-3-input').should(be.blank).type(cur_user.state_city.split(' ')[0]).press_enter()
-        browser.element('#react-select-4-input').should(be.blank).type(cur_user.state_city.split(' ')[-1]).press_enter()
+    def fill_state_city(self, current_user: User):
+        browser.element('#react-select-3-input').should(be.blank)\
+            .type(current_user.state_city.split(' ')[0]).press_enter()
+        browser.element('#react-select-4-input').should(be.blank)\
+            .type(current_user.state_city.split(' ')[-1]).press_enter()
         return self
 
     def submit(self):
@@ -70,34 +72,34 @@ class RegistrationPage:
         browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
         return self
 
-    def register(self, cur_user: User):
+    def register(self, current_user: User):
         (self
-            .fill_firstname(cur_user)
-            .fill_lastname(cur_user)
-            .fill_email(cur_user)
-            .gender_make_choice(cur_user)
-            .fill_phone_number(cur_user)
-            .fill_birthday(cur_user)
-            .fill_subjects(cur_user)
-            .hobbies_make_choice(cur_user)
-            .upload_picture(cur_user)
-            .fill_address(cur_user)
-            .fill_state_city(cur_user)
+            .fill_firstname(current_user)
+            .fill_lastname(current_user)
+            .fill_email(current_user)
+            .gender_make_choice(current_user)
+            .fill_phone_number(current_user)
+            .fill_birthday(current_user)
+            .fill_subjects(current_user)
+            .hobbies_make_choice(current_user)
+            .upload_picture(current_user)
+            .fill_address(current_user)
+            .fill_state_city(current_user)
             .submit())
         return self
 
-    def should_registered_with(self, cur_user: User):
+    def should_registered_with(self, current_user: User):
         self.form_should_be_completed()
         browser.element('.table').all('td').even.should(have.exact_texts(
-            cur_user.fullname,
-            cur_user.email,
-            cur_user.gender,
-            cur_user.phone_number,
-            cur_user.birthday.strftime('%d %B,%Y'),
-            cur_user.subject,
-            cur_user.hobby,
-            cur_user.photo.split('/')[-1],
-            cur_user.address,
-            cur_user.state_city
+            current_user.fullname,
+            current_user.email,
+            current_user.gender,
+            current_user.phone_number,
+            current_user.birthday.strftime('%d %B,%Y'),
+            current_user.subject,
+            current_user.hobby,
+            current_user.photo.split('/')[-1],
+            current_user.address,
+            current_user.state_city
         ))
         return self
