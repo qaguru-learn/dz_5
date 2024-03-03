0b9c81c03ffa6129b74f5afa1a7ad3cc1880faaf
legend23 <yu@vtechnology.ru>
2024-03-03 22:55:55 +0500

    change cur_user to current_user

--- demoqa_tests/models/pages/simple_registration_page.py
+++ demoqa_tests/models/pages/simple_registration_page.py
@@ -8,39 +8,39 @@ class SimpleRegistrationPage:
         browser.open('/text-box')
         return self
 
-    def fill_fullname(self, cur_user: SimpleUser):
-        browser.element('#userName').should(be.blank).type(cur_user.fullname)
+    def fill_fullname(self, current_user: SimpleUser):
+        browser.element('#userName').should(be.blank).type(current_user.fullname)
         return self
 
-    def fill_email(self, cur_user: SimpleUser):
-        browser.element('#userEmail').should(be.blank).type(cur_user.email)
+    def fill_email(self, current_user: SimpleUser):
+        browser.element('#userEmail').should(be.blank).type(current_user.email)
         return self
 
-    def fill_current_address(self, cur_user: SimpleUser):
-        browser.element('#currentAddress').should(be.blank).type(cur_user.current_address)
+    def fill_current_address(self, current_user: SimpleUser):
+        browser.element('#currentAddress').should(be.blank).type(current_user.current_address)
         return self
 
-    def fill_permanent_address(self, cur_user: SimpleUser):
-        browser.element('#permanentAddress').should(be.blank).type(cur_user.permanent_address)
+    def fill_permanent_address(self, current_user: SimpleUser):
+        browser.element('#permanentAddress').should(be.blank).type(current_user.permanent_address)
         return self
 
     def submit(self):
         browser.element('#submit').press_enter()
         return self
 
-    def register(self, cur_user: SimpleUser):
+    def register(self, current_user: SimpleUser):
         (self
-         .fill_fullname(cur_user)
-         .fill_email(cur_user)
-         .fill_current_address(cur_user)
-         .fill_permanent_address(cur_user)
+         .fill_fullname(current_user)
+         .fill_email(current_user)
+         .fill_current_address(current_user)
+         .fill_permanent_address(current_user)
          .submit()
          )
         return self
 
-    def should_registered_with(self, cur_user: SimpleUser):
-        browser.element('#name').matching(have.text(cur_user.fullname))
-        browser.element('#email').matching(have.text(cur_user.email))
-        browser.element('#currentAddress').matching(have.text(cur_user.current_address))
-        browser.element('#permanentAddress').matching(have.text(cur_user.permanent_address))
+    def should_registered_with(self, current_user: SimpleUser):
+        browser.element('#name').matching(have.text(current_user.fullname))
+        browser.element('#email').matching(have.text(current_user.email))
+        browser.element('#currentAddress').matching(have.text(current_user.current_address))
+        browser.element('#permanentAddress').matching(have.text(current_user.permanent_address))
         return self
